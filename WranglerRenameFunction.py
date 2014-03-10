f502f7c2d639b98bb85c3808f4de887638e6baa1
U-mn-01\Marcus <Marcus@mn-01.(none)>
2014-03-10 19:25:06 +0100

    Refactoring

--- WranglerRenameFunction.py
+++ WranglerRenameFunction.py
@@ -1,9 +1,8 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os
 
-class WranglerPromptRenameModuleCommand(sublime_plugin.WindowCommand):
-
+class WranglerPromptRenameFunctionCommand(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel("Wrangler Rename Function:", "", self.on_done, None, None)
+        self.window.show_input_panel("Wrangler New Function Name:", "", self.on_done, None, None)
         pass
 
     def on_done(self, text):
@@ -14,19 +13,27 @@ class WranglerPromptRenameModuleCommand(sublime_plugin.WindowCommand):
             pass
 
 class WranglerRenameFunctionCommand(sublime_plugin.TextCommand):
-	def run(self, edit, newName):
-		funName = "myfun"
-		arity = "0"
-		for region in self.view.sel():
-			if not region.empty():
-				line = view.line(region)
-				print(line)
+  def run(self, edit, newName):
+    funName = "myfun"
+    arity = 0
+    for region in self.view.sel():
+      if not region.empty():
+        lc = self.view.line(region)
+        line = self.view.substr(lc)
+        funName = self.view.substr(region)
+        if "()" in line:
+            arity = 0
+        else:
+            ## Need a regexp here to get the real correct arity
+            arity = line.count(',') + 1
+            print(arity)
 
-		filename = self.view.file_name()
-		cmd = "erl_call -a \"api_wrangler rename_fun [\\""" + "\"" + filename + "\\" + "\"" + "," + funName + "," + arity + "," +  newName + ", [] ]\" -sname wrangler"
-		self.view.window().run_command("exec", {
-       		"cmd": cmd,
-       		"shell": True,
-       		"encoding": "cp850",
-       		"file_regex": "([^ ]*\.erl):?(\d*)"
-       		})
+    filename = self.view.file_name()
+    filepath = os.path.dirname(filename)
+    cmd = "erl_call -a \"api_wrangler rename_fun [\\""\"{0}\\""\", {1}, {2}, {3}, [\\""\"{4}\\""\"] ]\" -sname wrangler".format(filename, funName, arity, newName, filepath)
+    self.view.window().run_command("exec", {
+          "cmd": cmd,
+          "shell": True,
+          "encoding": "cp850",
+          "file_regex": "([^ ]*\.erl):?(\d*)"
+          })
